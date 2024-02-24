465b2ba3795871d0eb9bdba6c64ae684ef66e971
jv88899 <jv88899@gmail.com>
2024-02-24 13:28:50 -0600

    created Description component

--- src/pages/add-album/AddAlbum.js
+++ src/pages/add-album/AddAlbum.js
@@ -1,9 +1,11 @@
+import Description from "../../components/description/Description";
 import Title from "../../components/title/Title";
 
 export default function AddAlbum() {
   return (
     <div>
       <Title text="Add Album" />
+      <Description text="This is the page where a user can add an album" />
     </div>
   );
 }
--- src/pages/view-album/ViewAlbum.js
+++ src/pages/view-album/ViewAlbum.js
@@ -1,9 +1,11 @@
+import Description from "../../components/description/Description";
 import Title from "../../components/title/Title";
 
 export default function ViewAlbum() {
   return (
     <div>
       <Title text="View Album" />
+      <Description text="This is the page where a user can view an album" />
     </div>
   );
 }
